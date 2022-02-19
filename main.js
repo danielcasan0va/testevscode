444e5558988d7a58c1c3c7cd02485d222dc5a694
danieldev <danil.s.c9090@gmail.com>
2022-02-19 14:15:09 -0300

    js.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function defineNomeUsuario() {
+    let meuNome = prompt('Por favor, digite seu nome.');
+    if(!meuNome || meuNome === null) {
+      defineNomeUsuario();
+    } else {
+      localStorage.setItem('nome', meuNome);
+      meuCabecalho.innerHTML = 'Mozilla é legal, ' + meuNome;
+    }
+  }
